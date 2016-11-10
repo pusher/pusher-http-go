dc7147e09d809e508a72276ceba57f177b919595
Vivan Kumar <vivangkumar@gmail.com>
2016-11-10 16:24:29 +0000

    Don't use method constants from http package
    This breaks compatibility with go 1.5

--- requests/requests.go
+++ requests/requests.go
@@ -1,35 +1,31 @@
 package requests
 
-import (
-	"net/http"
-)
-
 var Trigger = &Request{
-	Method:      http.MethodPost,
+	Method:      "POST",
 	PathPattern: "/apps/%s/events",
 }
 
 var TriggerBatch = &Request{
-	Method:      http.MethodPost,
+	Method:      "POST",
 	PathPattern: "/apps/%s/batch_events",
 }
 
 var Channels = &Request{
-	Method:      http.MethodGet,
+	Method:      "GET",
 	PathPattern: "/apps/%s/channels",
 }
 
 var Channel = &Request{
-	Method:      http.MethodGet,
+	Method:      "GET",
 	PathPattern: "/apps/%s/channels/%s",
 }
 
 var ChannelUsers = &Request{
-	Method:      http.MethodGet,
+	Method:      "GET",
 	PathPattern: "/apps/%s/channels/%s/users",
 }
 
 var NativePush = &Request{
-	Method:      http.MethodPost,
+	Method:      "POST",
 	PathPattern: "/server_api/v1/apps/%s/notifications",
 }
